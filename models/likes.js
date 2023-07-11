bcde82138d8211c58c6191a47fffb30f2efbd223
Kim Joohee <syap1170@gmail.com>
2023-07-11 10:40:21 +0900

    feat: 테이블 생성
    Users, Posts, Comments, Likes

---
+++ models/likes.js
@@ -0,0 +1,47 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Likes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Likes.init(
+    {
+      likeId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      UserId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      PostId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Likes',
+    }
+  );
+  return Likes;
+};
